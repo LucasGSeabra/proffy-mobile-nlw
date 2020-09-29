be330c185524488e5649bcdd77d3e411f562eb81
Lucas Seabra <lucasgseabra@gmail.com>
2020-09-29 09:49:21 -0300

    🖊 Adicionado landing page no appStack

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Landing from '../pages/Landing/';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -9,6 +10,7 @@ function AppStack() {
     return (
         <NavigationContainer>
             <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name="Landing" component={Landing} /> 
             </Navigator>
         </NavigationContainer>
     );
